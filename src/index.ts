5be2dc1abfc3d44a4646023153b87ded12fcd70d
Le Hoang Anh <anh.le@angelhack.com>
2025-03-12 10:46:36 +0700

    code: export Deployer and Provider under Env object

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { deploy } from "./core/deploy";
 import { upgrade } from "./core/upgrade";
 
+import { Deployer, Provider } from "./core/env";
+
 import { Utils } from "./libs/utils";
 
 export const DeployHelper = {
 	deploy,
 	upgrade,
 	Utils,
+	Env: {
+		Deployer,
+		Provider,
+	}
 };
